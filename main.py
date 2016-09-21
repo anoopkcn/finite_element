cb7d35f5a4da803f5e711603eecd3f5cd0999c0e
strivetobelazy <strivetobelazy@gmail.com>
2016-09-21 18:42:09 +0530

    added main

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from mesh import *
+from basis_func import *
+from assemble import *
+
+from viewer import *
+
+def clear_rows(A,b_nodes):
+    """ code to clera rows """
+
+
+
+if __name__ == "__main__":
