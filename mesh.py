099de124dc5d8f08e48e1cccba1140591f919e9d
gggsanna <giangiacomo.sanna@gmail.com>
2016-09-21 14:12:24 +0200

    working mesh

--- mesh.py
+++ mesh.py
@@ -1,9 +1,52 @@
 import numpy as np
 
+#output should be topo, x, y, b_nodes
+#TODO: nodes is not really updated (nor tested)
+#      
+
 def read_msh(filename):
 
-    """ read mesh code """
+    x = np.array([])
+    y = np.array([])
+    
+    topo = np.empty([0,3], dtype=int)
 
+    nodes = np.array([], dtype=int)
+    b_nodes = np.array([],dtype=int)
+        
+
+    
+
+    f = open(filename,'r')
+
+    for line in f:
+        l = line.split()
+        if line[0] == '$':
+            print "this is useless"
+        elif len(l) == 4:
+            l = map(float, l)
+            x = np.append(x, l[2])
+            y = np.append(y, l[3])
+        elif len(l) == 7:
+            l = map(int, l)
+            extremities = l[5:]
+            for i in extremities:
+                if i-1 not in b_nodes: #does this really exist? IT DOES
+                    b_nodes = np.append(b_nodes,i-1)    
+        elif len(l) == 8:
+            l = map(int, l)
+            topo_row = np.array(l[5:])
+            topo = np.vstack((topo, topo_row))
+            #print topo_row
+            #print type(topo)
+    
+    #numbering is off by one
+    topo = topo - 1
+    print topo #comment me out
+
+    f.close()
+
+    #now reverse the orientation, if needed
     r_id = 0
     for row in topo:
         ck =      (x[row[1]]-x[row[0]])*(y[row[2]]-y[row[0]])
--- test_suite.py
+++ test_suite.py
@@ -1,8 +1,8 @@
 from __future__ import division
 import unittest
 from mesh import *
-from basis_func import *
-from assemble import *
+#from basis_func import *
+#from assemble import *
 import numpy as np
 
 def almost_equal(a, b, digits):
@@ -27,6 +27,7 @@ def matrix_almost_equal(a,b,digits):
             return truth
     return truth
 
+"""
 class BasisFuncsTest(unittest.TestCase):
     def test_basis_func_00(self):
         x = np.array([0.,1.,0.])
@@ -44,7 +45,7 @@ class BasisFuncsTest(unittest.TestCase):
         self.assertTrue(lists_almost_equal(dx_phi, [0.,1.,-1.],1))
         self.assertTrue(lists_almost_equal(dy_phi, [-1.,1.,0.],1))
         self.assertTrue(lists_almost_equal(phi,[0.2 ,0.7, 0.1],1))
-
+"""
 
 class MeshTest(unittest.TestCase):
     def test_read_msh(self):
@@ -55,6 +56,7 @@ class MeshTest(unittest.TestCase):
         self.assertItemsEqual(b_nodes[:5], [0,4,5,6,7])
         # assertCountEqual for python 3
 
+"""
 class AssembleTest(unittest.TestCase):
     def test_assemble(self):
         #topo , x , y , nodes , b_nodes = read_msh('mesh/square.msh')
@@ -71,6 +73,7 @@ class AssembleTest(unittest.TestCase):
         self.assertTrue(truth)
         #self.assertItemsEqual(result, expected)
         #self.assertItemsEqual(b_nodes[:5], [0,4,5,6,7])
+"""
 
 if __name__ == "__main__":
     unittest.main()
