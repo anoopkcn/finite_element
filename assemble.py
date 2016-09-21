fixed assemble f_v, plots differencewith factor of two

assemble.py
@@ -59,8 +59,12 @@ def f_v(topo,x,y):
     for element in topo:
         x_l = x[element]
         y_l = y[element]
-        (dx_phi,dy_phi,phi,surf_e) = tri_p1(x_l,y_l,np.zeros((1,2)))
 
-    F=surf_e/3.*np.ones((x.shape[0]))
+        surf_e = 1./2. *\
+        abs(x_l[0]*y_l[2]-x_l[0]*y_l[1]+x_l[1]*y_l[0]-\
+        x_l[1]*y_l[2]+x_l[2]*y_l[1]-x_l[2]*y_l[0] )
+        l_F = surf_e/3. * force(x_l,y_l)
+        for i in range (0,3):
+            F[element[i]] += l_F[i]
 
     return F

main.py
@@ -37,4 +37,8 @@ if __name__ == "__main__":
     F[b_nodes]=0
     A_clear = clear_rows(A,b_nodes)
     sol = np.linalg.solve(A_clear,F)
-    plot_sol_p1(x,y,sol,topo)
+    #plot_sol_p1(x,y,sol,topo)
+    actual_sol = np.sin(np.pi * x) * np.sin(np.pi * y)
+    diff = 2*sol - actual_sol
+    plot_sol_p1(x,y,diff,topo)
+